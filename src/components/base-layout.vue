1c33297adf894fdcfba157a0c50fadcf3f8b8fdf
DESKTOP-RP4BFD4\AKI <ahsi.aki@gmail.com>
2023-01-24 16:37:33 +0800

    slot-practice

--- src/components/base-layout.vue
+++ src/components/base-layout.vue
@@ -4,35 +4,39 @@
 <template>
   <div class="wrapper">
     <aside class="sidebar">
-      <slot name="sidebar"/>
+      <slot name="sidebar" />
     </aside>
     <main class="main">
-      <slot name="main"/>
+      <slot name="main" />
     </main>
-    <footer class="footer">
-      <slot name="footer"/>
-    </footer>
   </div>
+  <footer class="footer">
+    <slot name="footer" />
+  </footer>
 </template>
 
 <style scoped>
-  .wrapper{
-    text-align: center;
-  }
-  .sidebar{
-    background-color: #960000;
-    color: aliceblue;
-    height: 20vh;
-  }
+.wrapper {
+  text-align: center;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+}
 
-  .main{
-    background-color: rgb(171, 74, 228);
-    color: aliceblue;
-    height: 70vh;
-  }
-  .footer{
-    background-color: rgb(74, 184, 228);
-    color: bisque;
-    height: 10vh;
-  }
+.sidebar {
+  background-color: #960000;
+  color: aliceblue;
+  height: 80vh;
+}
+
+.main {
+  background-color: rgb(171, 74, 228);
+  color: aliceblue;
+  height: 80vh;
+}
+
+.footer {
+  background-color: rgb(74, 184, 228);
+  color: bisque;
+  height: 20vh;
+}
 </style>
